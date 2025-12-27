ef8adf9c5d053a4e266816f86cd0ae63ed0bd96c
DaniilKalts <kaltsdaniil@gmail.com>
2025-12-27 19:03:12 +0500

    fix: copy screenshot

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -18,12 +18,12 @@ mkdir -p "$SCREENSHOT_DIR"
 case "$MODE" in
     fullscreen)
         # Fullscreen screenshot with satty annotation
-        grim -l 0 - | satty --filename -
+        grim -l 0 - | satty --filename - --notify-copied
         ;;
 
     region)
         # Region selection screenshot with satty annotation
-        grim -l 0 -g "$(slurp)" - | satty --filename -
+        grim -l 0 -g "$(slurp)" - | satty --filename - --notify-copied
         ;;
 
     fullscreen-save)
