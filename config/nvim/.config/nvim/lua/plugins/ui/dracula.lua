9332732a98fc087a3b552de13a9b3596d81a90df
DaniilKalts <kaltsdaniil@gmail.com>
2026-01-09 11:09:49 +0500

    feat: add dracula theme to neovim go syntax highlighting

--- config/nvim/.config/nvim/lua/plugins/ui/dracula.lua
+++ config/nvim/.config/nvim/lua/plugins/ui/dracula.lua
@@ -2,39 +2,42 @@ return {
   "Mofiqul/dracula.nvim",
   as = "dracula",
   config = function()
-    vim.defer_fn(function()
-      require("dracula").setup({
-        colors = {
-          bg = "#282a36",
-          fg = "#f8f8f2",
-          selection = "#44475a",
-          comment = "#6272a4",
-          red = "#ff5555",
-          orange = "#ffb86c",
-          yellow = "#f1fa8c",
-          green = "#50fa7b",
-          purple = "#bd93f9",
-          cyan = "#8be9fd",
-          pink = "#ff79c6",
-          bright_red = "#ff6e6e",
-          bright_green = "#69ff94",
-          bright_yellow = "#ffffa5",
-          bright_blue = "#d6acff",
-          bright_magenta = "#ff92df",
-          bright_cyan = "#a4ffff",
-          bright_white = "#ffffff",
-          menu = "#21222c",
-          visual = "#3e4451",
-          gutter_fg = "#4b5263",
-          nontext = "#3b4048",
-        },
-        show_end_of_buffer = false,
-        transparent_bg = false,
-        italic_comment = true,
-      })
-    end, 100) -- Delay to ensure colors load after Neo-tree
+    require("dracula").setup({
+      colors = {
+        bg = "#282A36",
+        fg = "#F8F8F2",
+        selection = "#44475A",
+        comment = "#6272A4",
+        red = "#FF5555",
+        orange = "#FFB86C",
+        yellow = "#F1FA8C",
+        green = "#50fa7b",
+        purple = "#BD93F9",
+        cyan = "#8BE9FD",
+        pink = "#FF79C6",
+        bright_red = "#FF6E6E",
+        bright_green = "#69FF94",
+        bright_yellow = "#FFFFA5",
+        bright_blue = "#D6ACFF",
+        bright_magenta = "#FF92DF",
+        bright_cyan = "#A4FFFF",
+        bright_white = "#FFFFFF",
+        menu = "#21222C",
+        visual = "#3E4452",
+        gutter_fg = "#4B5263",
+        nontext = "#3B4048",
+        white = "#ABB2BF",
+        black = "#191A21",
+      },
+      show_end_of_buffer = false,
+      transparent_bg = false,
+      italic_comment = true,
+    })
+
+    -- Apply the colorscheme
+    vim.cmd([[colorscheme dracula]])
 
     -- Apply custom highlights for Neo-tree
-    vim.cmd("highlight NeoTreeTitleBar guibg=#bd93f9 guifg=#21222c")
+    vim.cmd("highlight NeoTreeTitleBar guibg=#BD93F9 guifg=#21222C")
   end,
 }
